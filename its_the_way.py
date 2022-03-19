c8bcf2260ab7927f5861cf6abe171267d6218079
DvirPerkin <Dvirperkin@gmail.com>
2022-03-20 01:05:26 +0200

    Function name fix

--- its_the_way.py
+++ its_the_way.py
@@ -1,8 +1,8 @@
 import os
 
 
-def end_with_deep(path: str):
+def start_with_deep(path: str):
     return [file for file in os.listdir(path) if file.startswith("deep")]
 
 
-end_with_deep("C:\\Users\\dvirp\\OneDrive - edu.hac.ac.il\\Documents\\GitHub\\Notebooks-master\\week05\\images")
+start_with_deep("C:\\Users\\dvirp\\OneDrive - edu.hac.ac.il\\Documents\\GitHub\\Notebooks-master\\week05\\images")
