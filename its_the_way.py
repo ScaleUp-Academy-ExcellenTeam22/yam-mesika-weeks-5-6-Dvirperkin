5978f263a4ac09f9a11e028e6ec9a78d07cc0fe4
DvirPerkin <Dvirperkin@gmail.com>
2022-03-31 11:18:36 +0300

    Documentation has been edited

--- its_the_way.py
+++ its_the_way.py
@@ -2,6 +2,10 @@ import os
 
 
 def start_with_deep(dir_path: str) -> list[str]:
+    """
+    @param dir_path - A path to directory.
+    @return - A list the files that start with "deep" in thair name in the given directory.
+    """
     return [file for file in os.listdir(dir_path) if file.startswith("deep")]
 
 
--- vinaigrette.py
+++ vinaigrette.py
@@ -5,7 +5,11 @@ import sys
 
 def create_random_date_in_range(start_date, end_date):
     """This function get two Date objects and rand a new Date object
-     between the start_date to the end_date."""
+     between the start_date to the end_date.
+     @param start_date - A Date object that indicates the start date of the requested range.
+     @param end_date - A Date object that indicates the end date of the requested range.
+     @return - A Date object that indicates a random date between the start_date to end_date.
+     """
 
     random_date = rand_date(start_date, end_date - start_date)
 
@@ -17,13 +21,19 @@ def create_random_date_in_range(start_date, end_date):
 
 def rand_date(start_date, date_range):
     """This function gets Date object that indicate the start date and
-    a Date object build from difference between start_date to later Date object."""
+    a Date object build from difference between start_date to later Date object.
+    @param start_date - A Date object that indicates the start date of the requested range.
+    @param date_range - A delta between two Date object that give the range from the start date.
+    @return - A Date object that indicates a random date in the given range.
+    """
     return start_date + datetime.timedelta(days=random.randrange(date_range.days))
 
 
 def get_date_from_user():
     """This function gets a date from the user and return a Date object that construct
-     with the given date. the date format is: YYYY-MM-DD."""
+     with the given date. the date format is: YYYY-MM-DD.
+     @return - A Date object that indicates the given date by the user.
+     """
     return datetime.date.fromisoformat(input("Enter a date (YYYY-MM-DD):"))
 
 
